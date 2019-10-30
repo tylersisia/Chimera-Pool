70649652d2fc8fd324734b69498a914d6d640c6b
tylersisia <tyler@pdssystems.com>
2019-10-30 16:23:31 -0400

    Create config.js

---
+++ config.js
@@ -0,0 +1,27 @@
+
+var api = 'http://54.172.240.25:8117'
+
+var api_blockexplorer = 'http://54.172.240.25:11245'
+
+var poolHost = '54.172.240.25'
+
+var irc = 'irc.freenode.net/#poolhost'
+
+var email = 'support@poolhost.com'
+
+var cryptonatorWidget = ['{symbol}-BTC', '{symbol}-USD', '{symbol}-EUR']
+
+var easyminerDownload = 'https://github.com/zone117x/cryptonote-easy-miner/releases/'
+
+var blockchainExplorer = 'http://chainradar.com/{symbol}/block/{id}'
+
+var transactionExplorer = 'http://chainradar.com/{symbol}/transaction/{id}'
+
+var themeCss = 'themes/default-theme.css'
+
+var networkStat = {
+'cmra': [
+['bcn.mypool.online', 'http://bcn.mypool.online:8084'],
+['democats.org', 'http://pool.democats.org:7603']
+]
+}
